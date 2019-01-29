d6cc396672475915bb5ba0cc0a44723d7fcd7e1f
filip505 <filip505@yahoo.com>
2019-01-29 16:37:04 +0100

    finily setup is done

--- bla.mjs
+++
@@ -1,6 +0,0 @@
-import express from 'express'
-import http from 'http'
-
-let app = express()
-app = http.createServer(app)
-app.listen(5001)
--- dist/middleware/authorization.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-exports.module = {};
-//# sourceMappingURL=authorization.js.map
