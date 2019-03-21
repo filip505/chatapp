b1af6b9d389889bd81de1226c7f820a24d7f3b81
filip505 <filip505@yahoo.com>
2019-03-21 02:02:52 +0100

    circle ci

--- server/src/main/app.js
+++ server/src/main/app.js
@@ -1,50 +1,50 @@
 import '@babel/polyfill'
-const express = require('express')
-const app = express()
-const port = 5001
+// const express = require('express')
+// const app = express()
+// const port = 5001
 
-app.get('/ping', (req, res) => res.send(JSON.stringify(process.env)))
+// app.get('/ping', (req, res) => res.send(JSON.stringify(process.env)))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-// import '@babel/polyfill'
-// import { createConnection, } from 'typeorm'
-// import { User, Token, Message, Conversation, Subject } from './models'
-// //eb deploy Mynode-env
-// //eb init -p docker mynode
-// const config = {
-//   type: "postgres",
-//   host: (process.env.DB_HOST) ? process.env.DB_HOST : 'localhost',
-//   port: 5432,
-//   username: (process.env.DB_USER) ? process.env.DB_USER : 'node',
-//   password: (process.env.DB_PASS) ? process.env.DB_PASS : 'node',
-//   database: "node",
-//   synchronize: false,
-//   logging: true,
-//   migrations: ["./migration/*.js"],
-//   cli: {
-//     "migrationsDir": "migration"
-//   },
-//   entities: [
-//     User,
-//     Conversation,
-//     Subject,
-//     Token,
-//     Message
-//   ]
-// }
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+import '@babel/polyfill'
+import { createConnection, } from 'typeorm'
+import { User, Token, Message, Conversation, Subject } from './models'
+//eb deploy Mynode-env
+//eb init -p docker mynode
+const config = {
+  type: "postgres",
+  host: (process.env.DB_HOST) ? process.env.DB_HOST : 'localhost',
+  port: 5432,
+  username: (process.env.DB_USER) ? process.env.DB_USER : 'node',
+  password: (process.env.DB_PASS) ? process.env.DB_PASS : 'node',
+  database: "node",
+  synchronize: false,
+  logging: true,
+  migrations: ["./migration/*.js"],
+  cli: {
+    "migrationsDir": "migration"
+  },
+  entities: [
+    User,
+    Conversation,
+    Subject,
+    Token,
+    Message
+  ]
+}
 
-// console.log(process.env.DB_HOST)
-// console.log(process.env.DB_USER)
-// console.log(process.env.DB_PASS)
+console.log(process.env.DB_HOST)
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASS)
 
-// let connection
+let connection
 
-// createConnection(config).then(async function (newConnection) {
-//   connection = newConnection
-//   await require('./server').default(5001)
-// }, function (error) {
-//   console.log('error', error)
-// })
+createConnection(config).then(async function (newConnection) {
+  connection = newConnection
+  await require('./server').default(5001)
+}, function (error) {
+  console.log('error', error)
+})
 
-// export default connection
+export default connection
 
